616729c7fd903ce779a367880c0224d045a2d543
menaged <jomenaged@gmail.com>
2023-07-23 17:19:36 -0400

    Test

---
+++ js/64/one.js
@@ -0,0 +1,14 @@
+console.log('Hello World!');
+
+var age = 21;
+x = 5;
+console.log(age, x);
+age = "hello";
+console.log(age);
+
+
+// alert('File loaded')
+var theName prompt('What is your name?')
+console.log('Hello World!' + theName)
+console.log('Hello ${theName}' )
+
