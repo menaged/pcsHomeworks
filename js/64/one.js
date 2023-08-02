47e3d80558f73ed25a21b2e1fe7a51af9edb921d
menaged <jomenaged@gmail.com>
2023-08-02 12:00:56 -0400

    test

--- js/64/one.js
+++ js/64/one.js
@@ -1,14 +1,16 @@
-console.log('Hello World!');
+console.log('Hello World!juju');
 
-var age = 21;
-x = 5;
-console.log(age, x);
+var age = 21.5;
+console.log(age, typeof age);
 age = "hello";
-console.log(age);
+console.log(age, typeof age);
+
+var name = 'Joe';
+nam = 'donald';
+console.log (name);
 
 
-// alert('File loaded')
-var theName prompt('What is your name?')
-console.log('Hello World!' + theName)
-console.log('Hello ${theName}' )
+for (var i = 0; i < 10, i++) {
+    console.log(i);
 
+}
---
+++ js/65/scopes.js
@@ -0,0 +1,12 @@
+'use strict';
+
+console.log(x);
+var x = 5;
+
+console.log(x);
+
+function add(a, b){
+    return a + b;
+}
+console.log(add(5, 5));
+
